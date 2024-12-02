f64940f85792aad599214834c11021011bfa89fd
Takeshi Matsuya <tmatsuya@MacchanMBA13.local>
2024-12-03 01:34:48 +0900

    add run script

---
+++ run.sh
@@ -0,0 +1,9 @@
+echo \$ ./mul_div
+./mul_div
+echo ------------------------------------------
+for name in fumi hotaka junpei kemuniku kota macchan ryota-iso ryota-iso_v2
+do
+  echo \$ ./mul_div_$name
+  ./mul_div_$name
+  echo ------------------------------------------
+done
